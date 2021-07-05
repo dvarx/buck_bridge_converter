1872f3814e5b7ea93dc19ff37c6d5fefdc303b83
dvarx <59966971+dvarx@users.noreply.github.com>
2021-07-05 17:45:21 +0200

    initial commit

---
+++ dimensioing.m
@@ -0,0 +1,12 @@
+%% dimensioning buck stage
+ibuck_max=8;
+fsw_buck=60e3;
+t_on_off=200e-9;        %turn on /off time of MOSFET
+dt_buck=1/fsw_buck;
+dQ=ibuck_max*t_on_off;
+C_buck_in=10e-6;  
+dV_buck_in=dQ/C_buck_in;    %keep the voltage drop on the bypass cap of the buck input below 1V
+
+loss_tan=15*1e-4;
+Xc=1/(C_buck_in*2*pi*fsw_buck);
+esr=loss_tan*Xc;
